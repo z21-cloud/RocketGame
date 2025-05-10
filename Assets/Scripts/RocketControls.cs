330f13011476576752c70d151492fc6c59ac558e
Sergey Starostin <starostin371@gmail.com>
2025-05-10 21:26:09 +0300

    Added sound & level system
    - Death sound
    - Flying sound
    - Win sound
    - Scenes now change automatically
    - if player loses, the level reloads

--- Assets/Scripts/RocketControls.cs
+++ Assets/Scripts/RocketControls.cs
@@ -3,19 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
 
 public class RocketControls : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
     [SerializeField] private float rotationSpeed = 25f;
-    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip flySound;
+    [SerializeField] private AudioClip boomSound;
+    [SerializeField] private AudioClip winSound;
 
+    private enum State
+    {
+        Playing,
+        Death,
+        Win
+    }
+
+    private State state = State.Playing;
     private RocketInput input;
     private Rigidbody rb;
+    private AudioSource audioSource;
     // Start is called before the first frame update
     private void Awake()
     {
+        state = State.Playing;
         audioSource = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
         input = new RocketInput();
@@ -25,12 +39,17 @@ public class RocketControls : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        RocketLaunch();
-        RocketRotation();
+        if (state == State.Playing)
+        {
+            RocketLaunch();
+            RocketRotation();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Playing) return;
+
         switch (collision.gameObject.tag)
         {
             case "Safe":
@@ -40,15 +59,27 @@ public class RocketControls : MonoBehaviour
                 Debug.Log("+1 energy");
                 Destroy(collision.gameObject);
                 break;
-            default:
-                // Останавливаем звук и отключаем систему ввода
+            case "Finish":
+                state = State.Win;
                 audioSource.Stop();
+                audioSource.PlayOneShot(winSound);
+                StartCoroutine(LoadLevelWithDelay(1));
+                break;
+            default:
+                state = State.Death;
+                audioSource.Stop();
+                audioSource.PlayOneShot(boomSound);
                 input.Disable(); // Отключаем Input Actions
-
-                // Уничтожаем текущую ракету
-                Destroy(this.gameObject);
+                StartCoroutine(LoadLevelWithDelay(0));
                 break;
         }
+    }
+
+    private IEnumerator LoadLevelWithDelay(int sceneID, float delay = 2.5f)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneID);
+        Destroy(this.gameObject);
     }
 
     private void RocketLaunch()
@@ -58,7 +89,7 @@ public class RocketControls : MonoBehaviour
             rb.AddRelativeForce(Vector3.up * speed, ForceMode.Force);
             if (!audioSource.isPlaying)
             {
-                audioSource.Play();
+                audioSource.PlayOneShot(flySound);
             }
         }
         else
